a621b60c7080ce6c0b8c5d84ca6fa91e5e928dea
UMUTONI Aliance <umutonialiance22@gmail.com>
2026-06-04 09:42:27 +0200

    Restore official Supabase database URL

--- kina_backend/settings.py
+++ kina_backend/settings.py
@@ -90,22 +90,18 @@ WSGI_APPLICATION = 'kina_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASE_URL = os.getenv(
+    'DATABASE_URL',
+    'postgresql://postgres:Kinamusic1234_@db.nnampzeagporgufqptxh.supabase.co:5432/postgres'
+)
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
